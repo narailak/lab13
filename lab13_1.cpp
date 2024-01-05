1cc557b1c4d616af5d86da0abc5d11a8e04e6d81
narailak <153053579+narailak@users.noreply.github.com>
2024-01-05 12:09:35 +0700

    done

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -2,13 +2,14 @@
 #include <cmath>
 using namespace std;
 
-void stat(const double[],int,double[]);
+void stat(const double[], int, double[]);
 
-int main(){
-    double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
-    int N = sizeof(A)/sizeof(A[0]);
+int main()
+{
+    double A[] = {1.2, 3.5, 6.9, 7.8, 12.5, 0.5};
+    int N = sizeof(A) / sizeof(A[0]);
     double B[6];
-    stat(A,N,B);
+    stat(A, N, B);
     cout << "Arithmetic Mean = " << B[0];
     cout << "\nStandard Deviation = " << B[1];
     cout << "\nGeometric Mean = " << B[2];
@@ -16,4 +17,31 @@ int main(){
     cout << "\nMax = " << B[4];
     cout << "\nMin = " << B[5];
     return 0;
+}
+
+void stat(const double A[], int N, double B[])
+{
+    double sum = 0, sumsquare = 0, mix = 1, sumhar = 0;
+    double AM, GM, SD, HM, MAX = -9999999999999, MIN = 999999999999;
+    for (int i = 0; i < N; i++)
+    {
+        sum += A[i];
+        sumsquare += pow(A[i], 2);
+        mix *= A[i];
+        sumhar += 1 / A[i];
+        if (A[i] > MAX)
+            MAX = A[i];
+        if (A[i] < MIN)
+            MIN = A[i];
+    }
+    AM = sum / N;
+    B[0] = AM;
+    SD = sqrt((sumsquare / N) - (pow(AM, 2)));
+    B[1] = SD;
+    GM = pow(mix, 1.0 / N);
+    B[2] = GM;
+    HM = N / sumhar;
+    B[3] = HM;
+    B[4] = MAX;
+    B[5] = MIN;
 }
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -5,19 +5,60 @@ using namespace std;
 const int N = 30;
 const int M = 70;
 
-void updateImage(bool [][M],int,int,int);
+void updateImage(bool[][M], int, int, int);
 
-void showImage(const bool [][M]);
+void showImage(const bool[][M]);
 
 int main()
 {
     bool image[N][M] = {};
-    int s,x,y;
-    do{
+    int s, x, y;
+    do
+    {
         showImage(image);
         cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
-        updateImage(image,s,x,y);
-    }while(s != 0 || x != 0 || y != 0);
+        updateImage(image, s, x, y);
+    } while (s != 0 || x != 0 || y != 0);
     return 0;
+}
+
+void showImage(const bool image[][M])
+{
+    for (int i = 0; i < M + 2; ++i)
+        cout << "-";
+    cout << "\n";
+    for (int i = 0; i < N; ++i)
+    {
+        cout << "|";
+        for (int j = 0; j < M; ++j)
+        {
+            if (image[i][j] == true)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "|\n";
+    }
+
+    for (int i = 0; i < M + 2; ++i)
+    {
+        cout << '-';
+    }
+    cout << endl;
+}
+
+void updateImage(bool image[][M], int s, int x, int y)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            double distance = sqrt(pow(i - x, 2) + pow(j - y, 2));
+            if (distance <= s - 1)
+            {
+                image[i][j] = 1;
+            }
+        }
+    }
 }
